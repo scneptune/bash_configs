8a51776ee8b8d2eb7bcc9ef73469f233996a0450
scneptune <rmalcome@gmail.com>
2017-10-12 23:38:03 -0700

    added PGP_Key and updated more install features for first day

--- first_day_new_computer_setup.sh
+++ first_day_new_computer_setup.sh
@@ -10,4 +10,7 @@ wait $breupdatepid
 echo `/usr/local/bin/brew install rbenv nvm git git-flow bash-completion postgresql mysql`
 wait
 echo "Install of homebrew libaries complete!"
-
+echo "Now installing Karabiner Elements, key remapping (F**k the misplaced Fn Key!)"
+$(cwd)/scripts/build-and-install.sh | curl -fsSL https://github.com/tekezo/Karabiner-Elements/releases/tag/version_11.1.0 | tar xvz 
+wait 
+echo "Installed Karabiner Elements!"
